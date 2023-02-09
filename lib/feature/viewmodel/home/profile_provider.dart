c487bcce79edae7f71ed1900abf1c7fd827fdeb6
uigitdev <ui.gitdev@gmail.com>
2023-02-09 23:11:40 +0100

    Implemented user update function.

--- lib/feature/viewmodel/home/profile_provider.dart
+++ lib/feature/viewmodel/home/profile_provider.dart
@@ -1,8 +1,42 @@
 import 'package:note_selector/note_selector.dart';
 
-class ProfileProvider extends ChangeNotifier{
+enum ProfileErrorState { none, emptyField }
+
+class ProfileProvider extends ChangeNotifier {
+  final _repo = ProfileRepositoryImpl();
   final nameController = TextEditingController();
   final usernameController = TextEditingController();
   final emailController = TextEditingController();
   final saveButtonStreamHolder = StreamHolder<bool>(false);
+  final updateErrorStateStreamHolder = StreamHolder<ProfileErrorState>(ProfileErrorState.none);
+
+  void updateUser(UserModel userModel) async {
+    if (!saveButtonStreamHolder.data) {
+      saveButtonStreamHolder.addData(true);
+      if (userModel.isFillAttributes()) {
+        updateErrorStateStreamHolder.addData(ProfileErrorState.none);
+        final user = await _repo.updateUser(userModel).catchError((e) {
+          saveButtonStreamHolder.addData(false);
+          updateErrorStateStreamHolder.addError(e);
+          return null;
+        });
+        if (user != null) {
+          clearFields();
+          userLocator.userStreamHolder.addData(user);
+        }
+        saveButtonStreamHolder.addData(false);
+      } else {
+        saveButtonStreamHolder.addData(false);
+        updateErrorStateStreamHolder.addData(ProfileErrorState.emptyField);
+      }
+    }
+  }
+
+  void clearFields() {
+    nameController.clear();
+    usernameController.clear();
+    emailController.clear();
+  }
+
+  bool areFieldsEmpty() => nameController.text.isEmpty && usernameController.text.isEmpty && emailController.text.isEmpty;
 }
