db4551eeb3a4180eb30773ba8b9f5be5fbb0a7b4
uigitdev <ui.gitdev@gmail.com>
2023-02-09 22:25:02 +0100

    Created profile provider and store user values and progress button state.

--- lib/feature/viewmodel/home/profile_provider.dart
+++ lib/feature/viewmodel/home/profile_provider.dart
@@ -1,4 +1,8 @@
 import 'package:note_selector/note_selector.dart';
 
 class ProfileProvider extends ChangeNotifier{
+  final nameController = TextEditingController();
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final saveButtonStreamHolder = StreamHolder<bool>(false);
 }
