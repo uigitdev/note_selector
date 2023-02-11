345b379b84d36bd3267c2c1085d479578ba78b91
uigitdev <ui.gitdev@gmail.com>
2023-02-11 22:08:37 +0100

    Created note content page

--- lib/feature/view/home/widget/home_notes_content.dart
+++ lib/feature/view/home/widget/home_notes_content.dart
@@ -1,10 +1,49 @@
 import 'package:note_selector/note_selector.dart';
 
-class HomeNotesContent extends StatelessWidget {
+class HomeNotesContent extends StatefulWidget {
   const HomeNotesContent({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Notes content'));
+  State<HomeNotesContent> createState() => _HomeNotesContentState();
+}
+
+class _HomeNotesContentState extends State<HomeNotesContent> {
+  @override
+  void initState() {
+    context.read<NoteProvider>().initNotes();
+    super.initState();
   }
+
+  @override
+  Widget build(BuildContext context) => Consumer<NoteProvider>(
+        builder: (context, provider, _) => StreamHolderBuilder<List<NoteModel>?>(
+          streamHolder: provider.noteListStreamHolder,
+          builder: (context, state, noteList, error) {
+            switch (state) {
+              case StreamHolderState.none:
+                return const Center(
+                  child: CircularProgressIndicator(color: MTheme.accent),
+                );
+              case StreamHolderState.hasError:
+                return const ErrorBody();
+              case StreamHolderState.hasData:
+                return ListView.separated(
+                  padding: const EdgeInsets.only(
+                    top: Dimens.paddingListTopBottom,
+                    bottom: Dimens.paddingListTopBottom,
+                  ),
+                  itemCount: noteList!.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, position) => NoteItem(
+                    noteModel: noteList[position],
+                    position: position,
+                  ),
+                  separatorBuilder: (context, position) => const SizedBox(
+                    height: Dimens.paddingListTopBottom,
+                  ),
+                );
+            }
+          },
+        ),
+      );
 }
