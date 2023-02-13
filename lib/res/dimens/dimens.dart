c223457865ce64e7fb73a9608067d73c5cab73fb
uigitdev <ui.gitdev@gmail.com>
2023-02-13 20:31:26 +0100

    Added res

--- lib/res/dimens/dimens.dart
+++ lib/res/dimens/dimens.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 class Dimens {
   Dimens._();
 
@@ -23,4 +25,17 @@ class Dimens {
   static const double friendListItemHeight = 80;
   static const double friendListItemRadius = friendListItemHeight / 2;
   static const double friendPaddingTop = 5;
+
+  static const double skeletonFriendItemTextWidth = friendListItemHeight / 1.2;
+  static const double skeletonFriendItemTextHeight = 15;
+  static const double skeletonFriendItemTextRadius = skeletonFriendItemTextHeight / 2;
+  static const double skeletonFriendItemTextTopPadding = 10;
+
+  static double skeletonNoteItemTitleWidth(BuildContext context) => MediaQuery.of(context).size.width * 0.7;
+  static double skeletonNoteItemSubtitleWidth(BuildContext context) => MediaQuery.of(context).size.width * 0.4;
+  static const double skeletonNoteItemTextTopPadding = 10;
+  static const double skeletonNoteItemCheckboxHeight = 20;
+  static const double skeletonNoteItemTextHeight = 15;
+  static const double skeletonNoteItemTextRadius = skeletonNoteItemTextHeight / 2;
+  static const double skeletonNoteItemCheckboxRadius = skeletonNoteItemCheckboxHeight * 0.3;
 }
