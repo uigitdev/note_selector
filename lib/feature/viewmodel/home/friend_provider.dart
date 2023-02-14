449c66476aeb81bad4b53203f3a2dbaacc60ff72
uigitdev <ui.gitdev@gmail.com>
2023-02-14 19:26:14 +0100

    Added new method to friend repository and implemented it into friend provider.

--- lib/feature/viewmodel/home/friend_provider.dart
+++ lib/feature/viewmodel/home/friend_provider.dart
@@ -3,14 +3,21 @@ import 'package:note_selector/note_selector.dart';
 class FriendProvider extends ChangeNotifier {
   final _repo = FriendRepositoryImpl();
   final friendListStreamHolder = StreamHolder<List<FriendModel>?>(null);
+  final friendNoteListStreamHolder = StreamHolder<List<NoteModel>?>(null);
 
   void initFriendList() async {
     final friends = await _repo.getFriends().catchError((e) {
       friendListStreamHolder.addError(e);
       return null;
     });
-    if (friends != null) {
-      friendListStreamHolder.addData(friends);
-    }
+    if (friends != null) friendListStreamHolder.addData(friends);
+  }
+
+  void getFriendNoteList(FriendModel friendModel) async {
+    final noteList = await _repo.getFriendNote(friendModel).catchError((e) {
+      friendNoteListStreamHolder.addError(e);
+      return null;
+    });
+    if (noteList != null) friendNoteListStreamHolder.addData(noteList);
   }
 }
